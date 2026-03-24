7b374e9066fc9a62fa06f44fbe9bf5837257f42b
Vishal Prasad Singh <vishalprasadsingh4@gmail.com>
2026-03-24 21:06:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Pattern 16/pattern-16.java
@@ -0,0 +1,18 @@
+class Solution {
+
+    void printTriangle(int n) {
+        // code here
+        
+        int num = 65;
+        
+        for(int i=1; i<=n; i++)
+        {
+            for(int j=65; j<=num; j++)
+            {
+                System.out.print((char)num);
+            }
+            System.out.println();
+            num= num+1;
+        }
+    }
+}
